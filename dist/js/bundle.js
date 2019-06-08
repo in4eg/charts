bbc7b1553f85f900fcda33914b55e4ebe3a96d65
inna.k <in4egsmile@gmail.com>
2019-06-08 21:08:39 +0300

    round charts

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -23,37 +23,131 @@ ChartGraph = function(element) {
 
 
 	var canvasType = $(canvas).data('type');
-	var chartData = $(canvas).data('chart');
+	var chartData = {
+		"First": 10,
+		"Second": 14,
+		"Third": 2,
+		"Fourth": 12,
+		"Fifth": 18
+	};
 
-	console.log(JSON.parse(chartData))
+	console.log(chartData)
+
+
 
 	this.drawGraph = function() {
 
 		switch (canvasType) {
 			case 'radial':
 			console.log( 'radial' );
+			var total_value = 0;
+			var roundSize = 0.5;
+			for (var categ in chartData){
+				var val = chartData[categ];
+				total_value += val;
+			}
+
+			var start_angle = 0;
+			for (categ in chartData){
+				val = chartData[categ];
+				var slice_angle = 2 * Math.PI * val / total_value;
+
+				drawPieSlice(
+					ctx,
+					canvas.width/2,
+					canvas.height/2,
+					Math.min(canvas.width/2,canvas.height/2),
+					start_angle,
+					start_angle+slice_angle,
+					);
+
+				start_angle += slice_angle;
+			}
+
+			drawPieSlice(
+				ctx,
+				canvas.width/2,
+				canvas.height/2,
+				//drawing a white circle over the chart
+				roundSize * Math.min(canvas.width/2,canvas.height/2),
+				0,
+				2 * Math.PI,
+				"#fff"
+				);
 			break;
 			case 'linear':
 			console.log( 'linear' );
+
 			break;
 			case 'pie':
 			console.log( 'pie' );
+
+			var total_value = 0;
+			for (var categ in chartData){
+				var val = chartData[categ];
+				total_value += val;
+			}
+
+			var start_angle = 0;
+			for (categ in chartData){
+				val = chartData[categ];
+				var slice_angle = 2 * Math.PI * val / total_value;
+
+				drawPieSlice(
+					ctx,
+					canvas.width/2,
+					canvas.height/2,
+					Math.min(canvas.width/2,canvas.height/2),
+					start_angle,
+					start_angle+slice_angle,
+					);
+
+				start_angle += slice_angle;
+			}
+
+			// draw caption
+
+			for (categ in chartData){
+				val = chartData[categ];
+				slice_angle = 2 * Math.PI * val / total_value;
+				var pieRadius = Math.min(canvas.width/2,canvas.height/2);
+				var labelX = canvas.width/2 + (pieRadius / 2) * Math.cos(start_angle + slice_angle/2);
+				var labelY = canvas.height/2 + (pieRadius / 2) * Math.sin(start_angle + slice_angle/2);
+
+				// if (this.options.doughnutHoleSize){
+				// 	var offset = (pieRadius * this.options.doughnutHoleSize ) / 2;
+				// 	labelX = canvas.width/2 + (offset + pieRadius / 2) * Math.cos(start_angle + slice_angle/2);
+				// 	labelY = canvas.height/2 + (offset + pieRadius / 2) * Math.sin(start_angle + slice_angle/2);               
+				// }
+
+				var labelText = Math.round(100 * val / total_value);
+				ctx.fillStyle = "white";
+				ctx.font = "bold 20px Arial";
+				ctx.fillText(labelText+"%", labelX,labelY);
+				start_angle += slice_angle;
+			}
 			break;
 			default:
 			console.log( 'no chart type' );
 		}
+		var legend = $(canvas).data('legend');
+		this.drawLegend(chartData, $(canvas).data('legend'))
 	}
+
+
+	this.drawLegend = function (chartData, legendId) {
+		var legendHTML = "";
+		for (categ in chartData){
+			legendHTML += "<div><span style='display:inline-block;width:20px;background-color: #000;'>&nbsp;</span> "+categ+"</div>";
+		}
+		document.getElementById(legendId).innerHTML = legendHTML;
+	}
+
 
 	function drawLine(ctx, startX, startY, endX, endY){
 		ctx.beginPath();
 		ctx.moveTo(startX,startY);
 		ctx.lineTo(endX,endY);
-		ctx.stroke();
-	}
-
-	function drawArc(ctx, centerX, centerY, radius, startAngle, endAngle){
-		ctx.beginPath();
-		ctx.arc(centerX, centerY, radius, startAngle, endAngle);
 		ctx.stroke();
 	}
 
@@ -64,6 +158,12 @@ ChartGraph = function(element) {
 		ctx.arc(centerX, centerY, radius, startAngle, endAngle);
 		ctx.closePath();
 		ctx.fill();
+	}
+
+	function drawArc(ctx, centerX, centerY, radius, startAngle, endAngle){
+		ctx.beginPath();
+		ctx.arc(centerX, centerY, radius, startAngle, endAngle);
+		ctx.stroke();
 	}
 
 
